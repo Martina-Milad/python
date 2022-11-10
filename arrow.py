61896c5c41c7af94983f6433acbf3fc29d04c346
Martina Milad <67021162+Martina-Milad@users.noreply.github.com>
2022-11-10 23:53:02 +0200

    Create arrow.py

---
+++ arrow.py
@@ -0,0 +1,13 @@
+for i in range(0,5,1):
+	for j in range(i+1):
+		print("    * ", end="")
+	print("\n")
+	
+print("* * * * * * * * * * * * * * * * * * * * *")
+
+
+for i in range(6, 0, -1):    
+	for j in range(0, i - 1,1):  
+		print("    *", end=' ')  
+	print("\n")
+	
